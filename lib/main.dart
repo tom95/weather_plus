0bc7b8bcd0b2b81f4d8571258d73edb3e3f0b501
Tom Beckmann <tomjonabc@gmail.com>
2018-05-26 18:46:28 +0200

    display errors

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,20 @@ import 'weather_display.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  ThemeData _buildTheme(ThemeData base) {
+    return base.copyWith(
+        accentColor: Colors.red[400],
+        primaryColor: Colors.lightBlue[400],
+        primaryIconTheme: base.iconTheme.copyWith(opacity: 0.1)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: _buildTheme(ThemeData.light()),
       home: new MyHomePage(title: 'Flutter Page'),
     );
   }
