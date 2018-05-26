d3d858376904cc321111cab4fd08e11d7a316466
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-26 20:37:08 +0200

    Load data from Firebase

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,28 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Feed extends StatelessWidget {
   @override
-  build(BuildContext context ) {
+  build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-          padding: EdgeInsets.all(8.0),
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                  leading: Icon(Icons.cloud),
-                  title: Text(data[index]),
-                  trailing: Row(
-                    children: <Widget>[
-                      Text("42"),
-                      Icon(Icons.person)
-                    ]
-                  ))
+        body: new StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('baby').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) return new Text('Loading...');
+            final int messageCount = snapshot.data.documents.length;
+            return new ListView.builder(
+              itemCount: messageCount,
+              padding: EdgeInsets.all(8.0),
+              itemBuilder: (_, int index) {
+                final DocumentSnapshot document = snapshot.data.documents[index];
+
+                return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.cloud),
+                      title: Text(document['name']),
+                      trailing: Row(children: <Widget>[
+                          /*Text(document['votes']),*/ // TODO: Doesn't work.
+                          Icon(Icons.person)
+                      ])
+                    )
+                );
+              },
             );
-            },
-          itemCount: data.length,
+          },
         )
     );
   }
 }
-
-final List<String> data = <String>["The air sucks!", "Why it is so hot?", "Storm!", "This is a very very very very very very long title"];
