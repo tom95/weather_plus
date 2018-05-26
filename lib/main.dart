ff0e8b301ef101b9693d4a2e3807d7cc1453623f
Tom Beckmann <tomjonabc@gmail.com>
2018-05-26 22:01:55 +0200

    fix layout of appbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   ThemeData _buildTheme(ThemeData base) {
     return base.copyWith(
         accentColor: Colors.red[400],
-        primaryColor: Colors.lightBlue[400],
+        primaryColor: const Color(0xFF33691E),
         primaryIconTheme: base.iconTheme.copyWith(opacity: 0.1)
     );
   }
@@ -67,41 +67,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
         leading: Icon(Icons.wb_sunny),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
+      body: ListView(
           children: <Widget>[
             WeatherDisplay(),
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Feed()
           ],
-        ),
-      ),
+    ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
