35874800b724ec7e9079a5dc6b9a457b89bfb59b
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-27 01:00:31 +0200

    Show images in comments

--- lib/comment_display.dart
+++ lib/comment_display.dart
@@ -14,10 +14,26 @@ class CommentDisplay extends StatelessWidget {
           if (!snapshot.hasData) return Text('Loading...');
           return Column(
               children: snapshot.data.documents.map((DocumentSnapshot document) {
-                return Card(
-                  child: ListTile(
+                var cardChildren = <Widget>[
+                  ListTile(
                     leading: Icon(Icons.comment),
                     title: Text(document['text']),
+                  ),
+                ];
+
+                String imageName = document['image'];
+                if (imageName != null) {
+                  cardChildren.add(
+                    new Container(
+                        height: 200.0,
+                        child: new Image.network("https://firebasestorage.googleapis.com/v0/b/weatherplus-2bfb7.appspot.com/o/comment-images%2F"
+                        + imageName + "?alt=media"))
+                  );
+                }
+
+                return Card(
+                  child: new Column(
+                    children: cardChildren
                   )
                 );
               }
